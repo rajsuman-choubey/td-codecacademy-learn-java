d601e8ea1c2703863fdec0440e4d53241537b8b8
rajsuman-choubey <Rajsuman.choubey2022@gmail.com>
2022-06-20 18:03:20 +0200

    push_hello_world

---
+++ src/main/java/basicjava/Var_Initialization.java
@@ -0,0 +1,29 @@
+package basicjava;
+
+public class Var_Initialization {
+  static int m=100;//static variable
+  static int n= 100;
+  static int o= m+n;
+  static int p= m*n;
+  static int q= m/n;
+  static int r= m-n;
+
+
+
+  {
+    int n=90;//local variable
+    System.out.println("variable "+ o);
+    System.out.println("variable "+ p);
+    System.out.println("variable "+ q);
+    System.out.println("variable "+ r);
+    System.out.println(n);
+
+  }
+
+
+
+  }
+
+
+
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,14 @@
+
 package org.example;
+import basicjava.*;
 
 public class Main {
+String s = "Hello";//Global variable
 
   public static void main(String[] args) {
     System.out.println("Hello world!");
+  Var_Initialization v = new Var_Initialization();
+  Main m = new Main();
+    System.out.println(m.s);
   }
-  }
+}
